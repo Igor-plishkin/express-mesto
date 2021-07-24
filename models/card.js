3a40f46092f433a74c67b1cb2f3315266a74fdbd
Igor-plishkin <Plisha.jr@yandex.ru>
2021-07-25 01:36:14 +0500

    feat: card & user schemes

---
+++ models/card.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    // eslint-disable-next-line no-undef
+    type: ObjectId,
+    required: true,
+  },
+  likes: {
+    // eslint-disable-next-line no-undef
+    type: [ObjectId],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("card", cardSchema);
