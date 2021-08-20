491a38df60e151096b817cba654437ce7f3dca32
Igor-plishkin <Plisha.jr@yandex.ru>
2021-08-21 02:58:54 +0500

    add user router validate

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const { celebrate, Joi } = require("celebrate");
 const router = require("express").Router();
 const {
   getAllUsers,
@@ -9,8 +10,21 @@ const {
 
 router.get("/users", getAllUsers);
 router.get("/users/me", getUserInfo);
-router.get("/users/:userId", getUserById);
-router.patch("/users/me", patchUser);
-router.patch("/users/me/avatar", patchAvatar);
+router.get("/users/:userId", celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).alphanum(),
+  }),
+}), getUserById);
+router.patch("/users/me", celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), patchUser);
+router.patch("/users/me/avatar", celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required(),
+  }),
+}), patchAvatar);
 
 module.exports = router;
